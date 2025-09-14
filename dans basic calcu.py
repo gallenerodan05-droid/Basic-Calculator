15a89589b37ed365d7a0134455321f09806c804b
gallenerodan05-droid <gallenerodan05@gmail.com>
2025-09-14 20:51:38 +0800

    Python basic calculator added

---
+++ dans basic calcu.py
@@ -0,0 +1,26 @@
+q = input("Enter operator +, -, *, /: ")
+x = int(input("Enter number: "))
+b = int(input("Enter number: "))
+
+
+if q == "+":
+    print(x + b)
+
+elif q == "*":
+    print(x * b)
+    
+    
+    
+elif q == "/":
+    print(int( x / b) )
+    
+    
+elif q == "-":
+    print(x - b)
+    print("taenamo")
+    
+else:
+    print("Invalid Syntax")
+
+if q == "+":
+    print("atay plus tanginamo kupal")
